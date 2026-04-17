49e1eea095927e489210b8b7d7c7592a83f5080a
Breakerh <bhammer93@gmail.com>
2026-04-17 23:57:42 +0000

    fix live path

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-cd /root/warera/wareraNL-bot
+cd /home/warera/live/wareraNL-bot
 
 git pull origin main
-./.venv/bin/pip install -r requirements.txt
+./.venv/bin/pip install -e .
 
 sudo systemctl restart wareranl-bot
 sudo systemctl --no-pager --full status wareranl-bot
