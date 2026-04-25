85efdc36a7f5ebaf9fc75b94e586684502a398e2
breakerh <bhammer93@gmail.com>
2026-04-25 23:50:03 +0200

    debug test for embeds

--- cogs/general.py
+++ cogs/general.py
@@ -176,6 +176,7 @@ class General(commands.Cog, name="general"):
                 break  # only one animal reaction per message
         if "app.warera.io" not in content.lower():
             return
+        self.bot.logger.info("app.warera.io" not in content.lower(), content.lower())
         try:
             await message.edit(suppress=True)
             self.bot.logger.info(
